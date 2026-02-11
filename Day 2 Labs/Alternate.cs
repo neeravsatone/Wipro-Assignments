2ee694f0140e6ad75e901432529e6bf488ed507c
Neerav-prog <nsatone007@gmail.com>
2026-02-11 12:17:20 +0530

    trial commit

---
+++ Day 2 Labs/Alternate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Alternate
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a sentence:");
+            string sentence = Console.ReadLine();
+
+            string[] words = sentence.Split(' ');
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    char[] arr = words[i].ToCharArray();
+                    Array.Reverse(arr);
+                    Console.Write(arr);
+                }
+                else
+                {
+                    Console.Write(words[i]);
+                }
+
+                Console.Write(" ");
+            }
+        }
+    }
+}
---
+++ Day 2 Labs/Palindrome.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Palindrome
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a sentence:");
+            string sentence = Console.ReadLine();
+
+            string[] words = sentence.Split(' ');
+            int count = 0;
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                string word = words[i];
+                char[] arr = word.ToCharArray();
+                Array.Reverse(arr);
+                string reversed = new string(arr);
+
+                if (word.Equals(reversed, StringComparison.OrdinalIgnoreCase))
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine("Total Count of Palindrome Words : " + count);
+        }
+    }
+}
